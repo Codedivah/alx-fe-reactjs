76d2541a52e2124e5dbc535243f6c23a3d055fff
Fatimah Badmus <fatimahbadmus04@gmail.com>
2025-07-11 16:05:58 +0100

    Add WelcomeMessage component and basic App structure

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,15 @@
+import WelcomeMessage from './WelcomeMessage';
+function WelcomeMessage() {
+    return (
+        
+        <div>
+            <WelcomeMessage />
+            <h1>“Hello everyone, I am learning React at ALX!”.</h1>
+            <p>This is a simple JSX component.</p>
+            <p>Welcome to the ALX React App!</p>
+            <p>"I am learning about JSX!".</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
