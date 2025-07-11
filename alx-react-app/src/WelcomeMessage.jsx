b99537b935901475d6809f2c0f18e9a52380a111
Fatimah Badmus <fatimahbadmus04@gmail.com>
2025-07-11 16:19:23 +0100

    Refactor WelcomeMessage component and update App to include it

--- alx-react-app/src/WelcomeMessage.jsx
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -1,13 +1,9 @@
-import WelcomeMessage from './WelcomeMessage';
 function WelcomeMessage() {
     return (
-        
         <div>
-            <WelcomeMessage />
-            <h1>“Hello everyone, I am learning React at ALX!”.</h1>
+            <WelcomeMessage /> 
+            <h1>Welcome to ALX React APP!</h1>
             <p>This is a simple JSX component.</p>
-            <p>Welcome to the ALX React App!</p>
-            <p>"I am learning about JSX!".</p>
         </div>
     );
 }
