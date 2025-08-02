bed472fe1b1e42e0086b4b9d258c1157686328c3
Fatimah Badmus <fatimahbadmus04@gmail.com>
2025-08-03 00:26:41 +0100

    refactor: simplify fetchUserData function and remove unnecessary API key handling

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,37 +1,18 @@
 import axios from 'axios'
 
-const SEARCH_API_URL = 'https://api.github.com/search/users'
 
-export const fetchUserData = async (username, location = '', minRepos = '', page = 1) => {
-  const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY
+export const fetchUserData = (username, location, minRepos, page = 1, perPage = 30) => {
+  let query = ''
 
-  let query = `${username} in:login`
+  if (username) query += `${username} in:login`
   if (location) query += ` location:${location}`
-  if (minRepos) query += ` repos:>${minRepos}`
+  if (minRepos) query += ` repos:>=${minRepos}`
 
-  const searchRes = await axios.get(`${SEARCH_API_URL}?q=${encodeURIComponent(query)}&per_page=5&page=${page}`, {
-    headers: {
-      Authorization: `Bearer ${apiKey}`
-    }
+  return axios.get('https://api.github.com/search/users', {
+    params: {
+      q: query.trim(),
+      page,
+      per_page: perPage,
+    },
   })
-
-  const userItems = searchRes.data.items
-
-  if (!userItems.length) throw new Error('No users matched')
-
-  // Fetch full user profiles in parallel
-  const detailedProfiles = await Promise.all(
-    userItems.map(user =>
-      axios.get(user.url, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      }).then(res => res.data)
-    )
-  )
-
-  return {
-    users: detailedProfiles,
-    totalCount: searchRes.data.total_count
-  }
 }
